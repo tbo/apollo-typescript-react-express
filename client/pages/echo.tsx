065955543e1281649820d0e160eb4b9c56ab59a1
Thomas Bonk <tbo@cybo.biz>
2018-04-07 16:06:56 +0200

    Removed unnecessary dependencies

--- client/pages/echo.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import {IRoute} from '../router';
-
-const Echo = (props: {route: IRoute}) => (
-  <div>Hello, {props.route.params.name}!</div>
-);
-
-export default Echo;
